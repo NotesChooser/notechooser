883df8feebc173671f87d9de2a731f8140ff3cab
Dylan A. King <dylan.a.king@outlook.com>
2021-06-03 10:07:09 -0700

    serverside tweas

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //serverside
-const express = require('express');
-//express is a simple server library downloaded fron npm
+const PORT = 3000;
+const express = require('express'); // lightweight server library
 const app = express();
 
-const serverInit = function () { //runs when server starts listening on port 3000
-    console.log("Server started!")
+const serverInit = function () { // runs on server start
+    console.log(`Server started! http://localhost:${PORT}`);
 };
 
-app.listen(3000, serverInit);
-app.use(express.static("client")); //tells express to serve the ./client folder when it recieves a request
+app.listen(PORT, serverInit);
+app.use(express.static("client")); // serve ./client
